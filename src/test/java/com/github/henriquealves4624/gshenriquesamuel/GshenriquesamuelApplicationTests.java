7a42047976cb4d4c8eb9b1a5b71ceea625ef9a56
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-13 22:15:02 -0300

    feat: Add teste

--- src/test/java/com/github/henriquealves4624/gshenriquesamuel/GshenriquesamuelApplicationTests.java
+++ src/test/java/com/github/henriquealves4624/gshenriquesamuel/GshenriquesamuelApplicationTests.java
@@ -1,5 +1,6 @@
 package com.github.henriquealves4624.gshenriquesamuel;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,10 @@ class GshenriquesamuelApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@DisplayName("teste com erro")
+    @Test
+    void testGivenNewBot_whenCreate_thenFail() throws Exception{
+    }
 
 }
